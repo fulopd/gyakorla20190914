6bd87e70dfa1314f85ee82c58112db581e332326
Fülöp Dániel <fulopdaniel@gmail.com>
2019-09-14 13:03:22 +0200

    kesz ismetles

--- VezerlesiSzerkezetek/Program.cs
+++ VezerlesiSzerkezetek/Program.cs
@@ -11,7 +11,7 @@ namespace VezerlesiSzerkezetek
         static void Main(string[] args)
         {
             #region Elágazások: If-else
-
+            Console.WriteLine("Kérem irja be hogy true: ");
             var input = Console.ReadLine();
             bool ertek; //igaz-hamis
             // ha input = "true"
@@ -91,6 +91,69 @@ namespace VezerlesiSzerkezetek
             }
 
             #endregion
+
+            #region Ciklusok: Foreach
+
+            Console.WriteLine("\nForeach: ");
+            foreach (string item in feladat)
+            {
+                Console.WriteLine(item);
+            }
+
+            #endregion
+
+            #region Ciklusok: Do-while
+            
+            string jelszo = "";
+            Console.WriteLine("Kérem adja meg a jelszót:");
+
+            do
+            {
+                
+                var bill = Console.ReadKey(true);
+
+                if (bill.Key != ConsoleKey.Backspace && bill.Key != ConsoleKey.Enter)
+                {
+                    jelszo += bill.KeyChar;
+                    Console.Write("*");
+                }
+                else
+                {
+                    if (bill.Key == ConsoleKey.Backspace && jelszo.Length > 0)
+                    {
+                        jelszo = jelszo.Substring(0, (jelszo.Length - 1));
+                        Console.Write("\b \b"); //törlés konzolnál
+                    }
+                }               
+                
+
+            } while (jelszo != "admin");
+
+            Console.WriteLine("\nsikeres belépés");
+
+            #endregion
+
+            #region Ciklusok: While
+
+            Console.WriteLine("A program 5 mp múlva kilép!");
+
+            DateTime jelen = DateTime.Now;
+            DateTime jovo = jelen.AddSeconds(5);
+
+            int temp = jelen.Second;
+            while (jelen < jovo)
+            {
+                jelen = DateTime.Now;
+                if (jelen.Second != temp)
+                {
+                    temp = jelen.Second;
+                    Console.WriteLine(jelen);
+                }
+                
+            }
+            Environment.Exit(0);
+            #endregion
+
 
             Console.ReadKey();
         }
